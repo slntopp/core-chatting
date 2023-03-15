d2c7f7076026d27c65fedccfb6e1a798d9133f33
Mik Iwanowski <info@slnt-opp.xyz>
2023-03-15 20:46:07 +0100

    sample create chat method

--- server/server.go
+++ server/server.go
@@ -30,3 +30,13 @@ func (srv *ChatsAPIServer) List(ctx context.Context, _ *connect.Request[cc.Empty
 
 	return connect.NewResponse(&cc.Chats{Chats: chats}), nil
 }
+
+func (srv *ChatsAPIServer) Create(ctx context.Context, req *connect.Request[cc.Chat]) (*connect.Response[cc.Chat], error) {
+	requestor, err := Requestor(srv.AccountKey, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	chat, err := srv.Client.CreateChat(requestor, req.Msg)
+	return connect.NewResponse(chat), err
+}

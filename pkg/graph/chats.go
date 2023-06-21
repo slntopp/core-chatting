a299c5d9b1c0dae58089d1b3fa20f912ca34e436
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2023-06-21 13:59:43 +0300

    Complete chat controller

--- pkg/graph/chats.go
+++ pkg/graph/chats.go
@@ -31,14 +31,92 @@ func NewChatsController(logger *zap.Logger, db driver.Database) *ChatsController
 }
 
 func (c *ChatsController) Create(ctx context.Context, chat *cc.Chat) (*cc.Chat, error) {
-	return nil, nil
+	log := c.log.Named("Create")
+	log.Debug("Req received")
+
+	document, err := c.col.CreateDocument(ctx, chat)
+	if err != nil {
+		return nil, err
+	}
+
+	chat.Uuid = document.Key
+
+	_, err = c.col.UpdateDocument(ctx, chat.GetUuid(), chat)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return chat, nil
 }
+
 func (c *ChatsController) Update(ctx context.Context, chat *cc.Chat) (*cc.Chat, error) {
-	return nil, nil
+	log := c.log.Named("Update")
+	log.Debug("Req received")
+
+	_, err := c.col.UpdateDocument(ctx, chat.GetUuid(), chat)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return chat, nil
 }
+
+const getChatsQuery = `
+FOR c in Chats
+	RETURN c
+`
+
 func (c *ChatsController) List(ctx context.Context) ([]*cc.Chat, error) {
-	return nil, nil
+	log := c.log.Named("List")
+	log.Debug("Req received")
+
+	cur, err := c.db.Query(ctx, getChatsQuery, map[string]interface{}{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var chats []*cc.Chat
+
+	for cur.HasMore() {
+		var chat cc.Chat
+
+		_, err := cur.ReadDocument(ctx, &chat)
+		if err != nil {
+			return nil, err
+		}
+
+		chats = append(chats, &chat)
+	}
+
+	return chats, nil
 }
+
+const deleteChatMessages = `
+FOR m in Messages
+	FILTER m.chat == @chat
+	REMOVE m IN Messages
+`
+
 func (c *ChatsController) Delete(ctx context.Context, chat *cc.Chat) (*cc.Chat, error) {
-	return nil, nil
+	log := c.log.Named("Delete")
+	log.Debug("Req received")
+
+	_, err := c.col.RemoveDocument(ctx, chat.GetUuid())
+
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = c.db.Query(ctx, deleteChatMessages, map[string]interface{}{
+		"chat": chat.GetUuid(),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return chat, nil
 }

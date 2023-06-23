0c18dac9b0aede2daf5131f3de90432cd4e970fd
Mikita Iwanowski <info@slnt-opp.xyz>
2023-06-23 20:54:02 +0200

    handle send messages with keyboard shortcuts

--- plugin/src/views/Dashboard/Chats/Chat.vue
+++ plugin/src/views/Dashboard/Chats/Chat.vue
@@ -4,20 +4,41 @@
             <chat-header style="height: 5vh" />
         </template>
 
-        <n-scrollbar style="height: 80vh; max-width: 80%;">
+        <n-scrollbar style="height: 80vh; max-width: 80%;" v-if="messages.length > 0">
             <n-list-item v-for="message in messages">
                 <message-view :message="message" />
             </n-list-item>
         </n-scrollbar>
 
+        <n-space v-else style="height: 80vh; max-width: 80%;" justify="center" align="center">
+            <n-alert type="info" title="No Messages yet">
+                Use textarea below to send a message.
+            </n-alert>
+        </n-space>
+
         <template #footer>
             <n-space style="min-height: 10vh" align="end">
                 <n-avatar round size="medium">Me</n-avatar>
 
-                <n-input type="textarea" size="small" :autosize="{
-                    minRows: 2,
-                    maxRows: 5
-                }" style="min-width: 50vw;" placeholder="Type your message" v-model:value="current_message.content" />
+                <n-tooltip placement="top-end">
+                    <template #trigger>
+
+                        <n-input type="textarea" size="small" :autosize="{
+                            minRows: 2,
+                            maxRows: 5
+                        }" style="min-width: 50vw;" placeholder="Type your message"
+                            v-model:value="current_message.content"
+                            @keypress.ctrl.enter.exact="e => { e.preventDefault(); handle_send() }"
+                            @keypress.ctrl.shift.enter.exact="e => { e.preventDefault(); handle_send(Kind.ADMIN_ONLY) }" />
+                    </template>
+
+                    <ul>
+                        <li><kbd>Ctrl</kbd> + <kbd>Enter</kbd> to send message</li>
+                        <li v-if="chat?.role == Role.ADMIN"><kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>Enter</kbd> to send
+                            message as Admin Note</li>
+                    </ul>
+
+                </n-tooltip>
 
                 <n-space>
                     <n-button type="success" ghost circle size="small" @click="handle_send()">
@@ -25,7 +46,8 @@
                             <n-icon :component="SendOutline" />
                         </template>
                     </n-button>
-                    <n-button type="warning" ghost circle size="small" @click="handle_send(Kind.ADMIN_ONLY)">
+                    <n-button v-if="chat?.role == Role.ADMIN" type="warning" ghost circle size="small"
+                        @click="handle_send(Kind.ADMIN_ONLY)">
                         <template #icon>
                             <n-icon :component="ClipboardOutline" />
                         </template>
@@ -37,16 +59,16 @@
 </template>
 
 <script setup lang="ts">
-import { defineAsyncComponent, h, ref } from 'vue';
+import { defineAsyncComponent, h, ref, computed, watch } from 'vue';
 import {
-    NSpace, NButton, NIcon,
-    NAvatar, NText, NInput,
+    NSpace, NButton, NIcon, NTooltip,
+    NAvatar, NText, NInput, NAlert,
     NList, NListItem, NScrollbar, NDivider
 } from 'naive-ui';
 
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import { useCcStore } from '../../../store/chatting';
-import { Chat, Message, Kind } from '../../../connect/cc/cc_pb';
+import { Chat, Message, Kind, Role } from '../../../connect/cc/cc_pb';
 
 const SendOutline = defineAsyncComponent(() => import('@vicons/ionicons5/SendOutline'));
 const ClipboardOutline = defineAsyncComponent(() => import('@vicons/ionicons5/ClipboardOutline'));
@@ -54,12 +76,13 @@ const ClipboardOutline = defineAsyncComponent(() => import('@vicons/ionicons5/Cl
 const MessageView = defineAsyncComponent(() => import('../../../components/chats/message.vue'));
 
 const route = useRoute();
+const router = useRouter();
 
 const store = useCcStore()
 
-const chat = ref(store.chats.get(route.params.uuid as string))
-
-store.resolve([...chat.value!.users, ...chat.value!.admins])
+const chat = computed(() => {
+    return store.chats.get(route.params.uuid as string)
+})
 
 function chatHeader() {
     let users = chat.value!.users.map(uuid => store.users.get(uuid)?.title ?? 'Unknown')
@@ -98,7 +121,6 @@ async function get_messages() {
     let res = await store.get_messages(chat.value! as Chat)
     messages.value = res.messages
 }
-get_messages()
 
 const current_message = ref<Message>(new Message({
     chat: route.params.uuid as string,
@@ -107,12 +129,36 @@ const current_message = ref<Message>(new Message({
 
 async function handle_send(kind = Kind.DEFAULT) {
     current_message.value.kind = kind
+    current_message.value.chat = route.params.uuid as string
     await store.send_message(current_message.value as Message)
 
     current_message.value = new Message({
-        chat: route.params.uuid as string,
         content: '',
     })
     await get_messages()
 }
+
+async function load_chat() {
+    store.resolve([...chat.value!.users, ...chat.value!.admins])
+    get_messages()
+}
+
+watch(chat, load_chat)
+load_chat()
 </script>
+
+<style>
+kbd {
+    background-color: #eee;
+    border-radius: 3px;
+    border: 1px solid #b4b4b4;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2), 0 2px 0 0 rgba(255, 255, 255, 0.7) inset;
+    color: #333;
+    display: inline-block;
+    font-size: 0.85em;
+    font-weight: 700;
+    line-height: 1;
+    padding: 2px 4px;
+    white-space: nowrap;
+}
+</style>

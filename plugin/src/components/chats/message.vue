821b7c6cea637f80fec15708d2af32f85a5c9270
Mikita Iwanowski <info@slnt-opp.xyz>
2023-06-23 23:10:08 +0200

    refactor message to functional component + add message timestamp

--- plugin/src/components/chats/message.vue
+++ plugin/src/components/chats/message.vue
@@ -1,41 +1,16 @@
 <template>
-    <n-grid :x-gap="12" :style="container_style">
-        <n-gi :span="3" v-if="message.sender != store.me.uuid">
-            <avatar />
-        </n-gi>
-
-        <n-gi :span="20">
-            <n-space vertical :align="message.sender == store.me.uuid ? 'end' : 'start'">
-                <n-h2 style="margin: 0">
-                    <n-text>
-                        {{ sender() }}
-                    </n-text>
-                </n-h2>
-
-                <div v-html="content()">
-                </div>
-
-                <!-- <n-p v-for="line in message.content.split('\n')">
-                    {{ line }}
-                </n-p> -->
-            </n-space>
-        </n-gi>
-
-        <n-gi :span="2" v-if="message.sender == store.me.uuid">
-            <avatar />
-        </n-gi>
-        <n-gi v-if="message.kind == Kind.ADMIN_ONLY">
-            <n-icon :color="theme.warningColor" size="24" :component="ClipboardOutline" />
-        </n-gi>
-    </n-grid>
+    <render>
+        <div v-html="content()">
+        </div>
+    </render>
 </template>
 
 <script setup lang="ts">
-import { PropType, defineAsyncComponent, h, computed } from 'vue';
+import { PropType, defineAsyncComponent, h, computed, ref } from 'vue';
 import {
     NAvatar, NGrid,
     NGi, NIcon, NSpace,
-    NText, NH2, useThemeVars
+    NText, NH2, useThemeVars, NTooltip, NDivider
 } from 'naive-ui'
 
 import { Message, Kind } from '../../connect/cc/cc_pb'
@@ -74,7 +49,24 @@ function sender(): string {
 }
 
 function avatar() {
-    return h(NAvatar, { round: true, size: 64 }, () => h('span', { style: { fontSize: '1.5rem' } }, sender()[0]))
+
+    let elements = [
+        h(NAvatar, { round: true, size: 64 }, () => h('span', { style: { fontSize: '1.5rem' } }, sender()[0]))
+    ]
+
+    if (message.kind == Kind.ADMIN_ONLY) {
+        elements.push(h(
+            NGi,
+            {},
+            () => h(NIcon, {
+                color: theme.value.warningColor,
+                size: 24,
+                component: ClipboardOutline,
+            }),
+        ))
+    }
+
+    return h(NSpace, { vertical: true, justify: 'start', align: 'center' }, () => elements)
 }
 
 const container_style = computed(() => {
@@ -84,12 +76,14 @@ const container_style = computed(() => {
     return { maxWidth: '98%', padding: '12px 0 12px 12px', borderRadius: theme.value.borderRadius, border: `1px solid ${theme.value.borderColor}` }
 })
 
-hljs.registerLanguage('javascript', javascript)
-hljs.registerLanguage('typescript', typescript)
-hljs.registerLanguage('css', css)
-hljs.registerLanguage('xml', xml)
-hljs.registerLanguage('json', json)
-hljs.registerLanguage('markdown', markdown)
+{
+    hljs.registerLanguage('javascript', javascript)
+    hljs.registerLanguage('typescript', typescript)
+    hljs.registerLanguage('css', css)
+    hljs.registerLanguage('xml', xml)
+    hljs.registerLanguage('json', json)
+    hljs.registerLanguage('markdown', markdown)
+}
 
 marked.use({
     async: false, gfm: true,
@@ -111,6 +105,115 @@ function content() {
     const sanitized = DOMPurify.sanitize(parsed)
 
     return sanitized
+}
+
+const now = ref(new Date())
+setInterval(() => now.value = new Date(), 1000)
+
+function getRelativeTime(timestamp: number) {
+    const timeDifference = (now.value.getTime() - timestamp) / 1000;
+    const minutesDifference = Math.floor(timeDifference / 60);
+
+    if (minutesDifference >= 4320) {
+        return new Date(timestamp).toLocaleDateString();
+    } else if (minutesDifference >= 1440) {
+        const daysDifference = Math.floor(minutesDifference / 1440);
+        return `${daysDifference} days ago`;
+    } else if (minutesDifference >= 60) {
+        const hoursDifference = Math.floor(minutesDifference / 60);
+        return `${hoursDifference} hours ago`;
+    } else if (minutesDifference > 0) {
+        return `${minutesDifference} minutes ago`;
+    } else {
+        return 'just now';
+    }
+}
+
+
+function timestamp() {
+
+    let result = ''
+    if (message.edited) {
+        result = 'edited, '
+    }
+
+    result += getRelativeTime(Number(message.edited ? message.edited : message.sent))
+
+    let tooltip = [
+        h(NDivider, { titlePlacement: 'left' }, 'Sent'),
+        h(NText, {}, () => new Date(Number(message.sent)).toString())
+    ]
+
+    if (message.edited) {
+        tooltip.push(
+            h(NDivider, { titlePlacement: 'left' }, 'Edited'),
+            h(NText, {}, () => new Date(Number(message.edited)).toString())
+        )
+    }
+
+    return h(NTooltip, {
+        placement: 'top'
+    }, {
+        trigger: () => h(NText, { depth: 3 }, () => result),
+        default: () => tooltip
+    })
+}
+
+function render(_props: any, { slots }: any) {
+
+    const is_sender = message.sender == store.me.uuid
+
+    const avatar_item = h(NGi, {
+        span: 3,
+    },
+        () => avatar()
+    )
+
+
+    let title = [
+        h(
+            NH2,
+            {
+                style: "margin: 0",
+            },
+            () => h(NText, {}, () => sender())
+        ),
+        timestamp()
+    ]
+
+    if (is_sender) {
+        title = title.reverse()
+    }
+
+    let elements = [
+        avatar_item,
+        h(NGi, { span: 21 },
+            () => h(
+                NSpace,
+                {
+                    vertical: true,
+                    align: is_sender ? "end" : "start",
+                },
+                () => [
+                    h(NSpace, { align: 'center' }, () => title),
+                    slots.default(),
+                ]
+            )
+        ),
+    ]
+
+    if (is_sender) {
+        elements = elements.reverse()
+    }
+
+    return h(
+        NGrid,
+        {
+            xGap: 12,
+            style: container_style.value,
+        },
+        () => elements
+    );
 }
 </script>
 

28d7e105373ab89aa3c20dcf205438eb4f60daff
Mikita Iwanowski <info@slnt-opp.xyz>
2023-07-03 20:02:54 +0200

    fix: vue-router plugin root

--- plugin/src/router/index.ts
+++ plugin/src/router/index.ts
@@ -2,36 +2,43 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: "/",
-    name: "Entrypoint",
-    component: () => import("../views/Entrypoint.vue"),
-  },
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    component: () => import("../views/Dashboard.vue"),
+    path: "/chatting",
+    name: "Root",
     children: [
       {
-        path: '',
-        name: 'Chats',
-        component: () => import("../views/Dashboard/Chats.vue"),
+        path: "",
+        name: "Entrypoint",
+        component: () => import("../views/Entrypoint.vue"),
+      },
+      {
+        path: "/dashboard",
+        name: "Dashboard",
+        component: () => import("../views/Dashboard.vue"),
         children: [
           {
             path: '',
-            name: 'Empty Chat',
-            component: () => import("../views/Dashboard/Chats/Empty.vue"),
-          },
-          {
-            path: 'start',
-            name: 'Start Chat',
-            component: () => import("../views/Dashboard/Chats/Start.vue"),
-          },
-          {
-            path: ':uuid',
-            name: 'Chat',
-            component: () => import("../views/Dashboard/Chats/Chat.vue"),
+            name: 'Chats',
+            component: () => import("../views/Dashboard/Chats.vue"),
+            children: [
+              {
+                path: '',
+                name: 'Empty Chat',
+                component: () => import("../views/Dashboard/Chats/Empty.vue"),
+              },
+              {
+                path: 'start',
+                name: 'Start Chat',
+                component: () => import("../views/Dashboard/Chats/Start.vue"),
+              },
+              {
+                path: ':uuid',
+                name: 'Chat',
+                component: () => import("../views/Dashboard/Chats/Chat.vue"),
+              }
+            ]
           }
         ]
+
       }
     ]
   }

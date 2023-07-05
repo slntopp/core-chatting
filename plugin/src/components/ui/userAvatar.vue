fc9bfeeebe4334a7b13f3967ecf70f945a42c367
PavelSazukin <sazukinpavelal@gmail.com>
2023-07-05 22:39:19 +0400

    add user avatar

---
+++ plugin/src/components/ui/userAvatar.vue
@@ -0,0 +1,24 @@
+<template>
+  <n-avatar :round="round" :size="size">{{ avatarPreview }}</n-avatar>
+</template>
+
+<script setup lang="ts">
+import {NAvatar} from "naive-ui";
+import {computed} from "vue";
+
+interface AvatarProps{
+  size:'medium' | 'large' | 'small'
+  avatar:string
+  round:boolean
+}
+
+const props = defineProps<AvatarProps>()
+
+const avatarPreview=computed(()=>{
+  return props.avatar?.split(" ").map((s:string)=>s?.[0]).join('').toUpperCase()
+})
+</script>
+
+<style scoped>
+
+</style>

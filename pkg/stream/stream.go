5cbff7556e5c4e138746d100c7eec4c28e356cf0
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2023-07-06 15:15:21 +0300

    Remove msg update in stream

--- pkg/stream/stream.go
+++ pkg/stream/stream.go
@@ -92,14 +92,14 @@ start_stream:
 
 			log.Info("Receive message", zap.Any("event", event))
 
-			if event.GetType() == cc.EventType_MESSAGE_SENT {
+			/*if event.GetType() == cc.EventType_MESSAGE_SENT {
 				message := event.GetMsg()
 				message.Readers = append(message.GetReaders(), requestor)
 				_, err := s.msgCtrl.Update(ctx, message)
 				if err != nil {
 					log.Error("Failed to update message", zap.Error(err))
 				}
-			}
+			}*/
 
 			err = serverStream.Send(event)
 			if err != nil {

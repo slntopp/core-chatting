d1517423b430e2e2dfb6c0fffb99d9966c23f202
PavelSazukin <sazukinpavelal@gmail.com>
2023-07-10 11:24:54 +0400

    refactor chat options

--- plugin/src/components/chats/chat_options.vue
+++ plugin/src/components/chats/chat_options.vue
@@ -3,7 +3,7 @@
     <n-space v-if="!isEdit" justify="start" align="center">
       <n-button round quaternary @click="router.push({ name: 'Empty Chat' })">
         <template #icon>
-          <n-icon :component="CloseSharp" />
+          <n-icon :component="CloseSharp"/>
         </template>
       </n-button>
 
@@ -16,20 +16,19 @@
       <n-space justify="center">
         <n-form :model="chat" ref="form" :rules="rules" label-placement="left">
           <n-form-item label="Topic">
-            <n-input v-model:value="chat.topic" clearable placeholder="What are we chatting about?" />
+            <n-input v-model:value="chat.topic" clearable placeholder="What are we chatting about?"/>
           </n-form-item>
 
           <n-form-item label="Members">
-            <n-select :render-tag="renderTag" v-model:value="chat.users" multiple :options="membersWithoutDuplicates"
-              filterable />
+            <member-select v-model:value="chat.users" :options="membersWithoutDuplicates"/>
           </n-form-item>
 
           <n-form-item label="Admins">
-            <n-select v-model:value="chat.admins" multiple :options="adminsWithoutDuplicates" filterable />
+            <n-select v-model:value="chat.admins" multiple :options="adminsWithoutDuplicates" filterable/>
           </n-form-item>
 
           <n-form-item label="Gateways">
-            <n-select v-model:value="chat.gateways" multiple :options="gateways_options" filterable />
+            <n-select v-model:value="chat.gateways" multiple :options="gateways_options" filterable/>
           </n-form-item>
         </n-form>
       </n-space>
@@ -45,7 +44,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineAsyncComponent, h, onMounted, ref, toRefs } from 'vue';
+import {computed, defineAsyncComponent, onMounted, ref, toRefs} from 'vue';
 import {
   FormInst,
   NButton,
@@ -56,15 +55,15 @@ import {
   NSelect,
   NSpace,
   NSpin,
-  NTag,
   NText,
   SelectOption,
-  SelectRenderTag
 } from 'naive-ui';
 
-import { useRouter } from 'vue-router';
-import { useCcStore } from "../../store/chatting.ts";
-import {Chat, Defaults, Role, Users} from "../../connect/cc/cc_pb";
+import {useRouter} from 'vue-router';
+import {useCcStore} from "../../store/chatting.ts";
+import {Chat, Role} from "../../connect/cc/cc_pb";
+import MemberSelect from "../users/member_select.vue";
+import useDefaults from "../../hooks/useDefaults.ts";
 
 interface ChatOptionsProps {
   isEdit?: boolean
@@ -74,12 +73,13 @@ interface ChatOptionsProps {
 const CloseSharp = defineAsyncComponent(() => import('@vicons/ionicons5/CloseSharp'));
 
 const props = defineProps<ChatOptionsProps>()
-const { isEdit, chat: oldChat } = toRefs(props)
+const {isEdit, chat: oldChat} = toRefs(props)
 
 const emit = defineEmits(['close'])
 
 const router = useRouter();
 const store = useCcStore();
+const {admins, fetch_defaults, isDefaultLoading, users, gateways} = useDefaults()
 
 const form = ref<FormInst>()
 const rules = {
@@ -88,7 +88,6 @@ const rules = {
   }
 }
 
-const isDefaultLoading = ref(false)
 const isEditLoading = ref(false)
 
 const chat = ref<Chat>(new Chat({
@@ -102,61 +101,49 @@ const gateways_options = ref<SelectOption[]>([])
 const members_options = ref<SelectOption[]>([])
 
 const adminsWithoutDuplicates = computed(() => admins_options.value.map(op => {
-  const disabled=!!chat.value.users.find(m => m === op.value)
-  return {...op,disabled}
+  const disabled = !!chat.value.users.find(m => m === op.value)
+  return {...op, disabled}
 }))
 
 const membersWithoutDuplicates = computed(() => members_options.value.map(op => {
-  const disabled=!!chat.value.admins.find(m => m === op.value)
-  return {...op,disabled}
+  const disabled = !!chat.value.admins.find(m => m === op.value)
+  return {...op, disabled}
 }))
 
-const me=computed(()=>store.me)
+const me = computed(() => store.me)
 
 onMounted(() => {
   if (isEdit?.value && oldChat?.value) {
-    chat.value = { ...oldChat.value } as Chat
+    chat.value = {...oldChat.value} as Chat
   }
 })
 
-async function fetch_defaults() {
-  try {
-    isDefaultLoading.value = true
+onMounted(async () => {
+  await fetch_defaults()
 
-    const data=await Promise.all([store.get_members(),store.fetch_defaults()])
-    const members=data[0] as Users
-    const defaults=data[1] as Defaults
+  admins_options.value = admins.value.map(admin => {
+    return {
+      label: store.users.get(admin)?.title ?? 'Unknown',
+      value: admin,
+    }
+  })
 
-    console.debug("Defaults", defaults)
+  gateways_options.value = gateways.value.map(gateway => {
+    return {
+      label: gateway,
+      value: gateway,
+    }
+  })
+  chat.value.gateways = gateways.value
 
-    admins_options.value = defaults.admins.map(admin => {
-      return {
-        label: store.users.get(admin)?.title ?? 'Unknown',
-        value: admin,
-      }
-    })
-
-    gateways_options.value = defaults.gateways.map(gateway => {
-      return {
-        label: gateway,
-        value: gateway,
-      }
-    })
-    chat.value.gateways = defaults.gateways
-
-    members_options.value = members.users.map(user => {
-      return {
-        label: user.title,
-        value: user.uuid,
-        disabled:me.value.uuid!==user.uuid && defaults.admins.includes(user.uuid),
-      }
-    })
-  } finally {
-    isDefaultLoading.value = false
-  }
-}
-
-fetch_defaults()
+  members_options.value = users.value.map(user => {
+    return {
+      label: user.title,
+      value: user.uuid,
+      disabled: me.value.uuid !== user.uuid && admins.value.includes(user.uuid),
+    }
+  })
+})
 
 function submit() {
   form.value!.validate(async (errs) => {
@@ -175,7 +162,7 @@ function submit() {
       } else {
         let result = await store.create_chat(chat.value as Chat);
 
-        router.push({ name: 'Chat', params: { uuid: result.uuid } })
+        router.push({name: 'Chat', params: {uuid: result.uuid}})
       }
     } finally {
       isEditLoading.value = false
@@ -183,24 +170,6 @@ function submit() {
 
 
   })
-}
-
-const renderTag: SelectRenderTag = ({ option, handleClose }) => {
-  return h(
-    NTag,
-    {
-      type: option.type as 'success' | 'warning' | 'error',
-      closable: true,
-      onMousedown: (e: FocusEvent) => {
-        e.preventDefault()
-      },
-      onClose: (e: MouseEvent) => {
-        e.stopPropagation()
-        handleClose()
-      }
-    },
-    { default: () => option.value !== option.label ? option.label : 'Deleted' }
-  )
 }
 </script>
 

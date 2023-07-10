98c81a753acf97accc2cf58cc91e04fb66318e35
PavelSazukin <sazukinpavelal@gmail.com>
2023-07-10 11:25:58 +0400

    add members dropdownb & select

---
+++ plugin/src/components/users/member_select.vue
@@ -0,0 +1,40 @@
+<template>
+  <n-select :render-tag="renderTag" :value="value" @update-value="emit('update:value',$event)" multiple :options="options"
+            filterable/>
+</template>
+
+<script setup lang="ts">
+import {NSelect, NTag, SelectRenderTag} from "naive-ui";
+import {h} from "vue";
+import {User} from "../../connect/cc/cc_pb.ts";
+
+interface MemberSelectProps {
+  options: User[]
+  value: string[]
+}
+
+const props = defineProps<MemberSelectProps>()
+const emit = defineEmits(['update:value'])
+
+const renderTag: SelectRenderTag = ({option, handleClose}) => {
+  return h(
+      NTag,
+      {
+        type: option.type as 'success' | 'warning' | 'error',
+        closable: true,
+        onMousedown: (e: FocusEvent) => {
+          e.preventDefault()
+        },
+        onClose: (e: MouseEvent) => {
+          e.stopPropagation()
+          handleClose()
+        }
+      },
+      {default: () => option.value !== option.label ? option.label : 'Deleted'}
+  )
+}
+</script>
+
+<style scoped>
+
+</style>
--- plugin/src/components/users/membersDropdown.vue
+++ plugin/src/components/users/membersDropdown.vue
@@ -5,10 +5,11 @@
 </template>
 
 <script setup lang="ts">
-import {NDropdown, NText,DropdownOption} from "naive-ui";
+import {NDropdown, NText, DropdownOption} from "naive-ui";
 import {User} from "../../connect/cc/cc_pb.ts";
 import {computed, h, toRefs, VNode} from "vue";
 import memberItem from './userItem.vue'
+import AddButton from "../ui/add_button.vue";
 
 interface MembersDropdownProps{
   members:User[]
@@ -17,18 +18,27 @@ interface MembersDropdownProps{
 const props=defineProps<MembersDropdownProps>()
 const {members}=toRefs(props)
 
-const emit=defineEmits(['delete'])
+const emit=defineEmits(['delete','add'])
 
 const membersOptions=computed(()=>{
-    return [...new Set(members.value)].map((m) => ({
+    const membersOptions= [...new Set(members.value)].map((m) => ({
       key: m.uuid,
       extra:m,
       label:m.title || 'Unknown',
     }))
+  membersOptions.push({key:'add',label:'add',extra: {} as User})
+  return membersOptions
 })
 
 const renderOption=({option}:{option:DropdownOption})=>{
+  if(option.key==='add'){
+    return h(AddButton,{style:{width:'100%'},onClick:addMember},'Add')
+  }
   return h(memberItem,{user:option.extra,onDelete:()=>emit('delete',option.key)})
+}
+
+const addMember=()=>{
+  emit('add')
 }
 </script>
 

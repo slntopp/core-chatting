4461f9b4e082535f9f88917fba74c6121d0f2d15
PavelSazukin <sazukinpavelal@gmail.com>
2023-07-10 11:25:06 +0400

    add ui add button

---
+++ plugin/src/components/ui/add_button.vue
@@ -0,0 +1,18 @@
+<template>
+  <n-button :bordered="false" @click="emit('click')"><slot></slot>
+    <n-icon style="margin-left: 5px" size="20">
+      <add-icon></add-icon>
+    </n-icon>
+  </n-button>
+</template>
+
+<script setup lang="ts">
+import {NButton,NIcon} from "naive-ui";
+import {AddCircle as AddIcon} from '@vicons/ionicons5'
+
+const emit=defineEmits(['click'])
+</script>
+
+<style scoped>
+
+</style>

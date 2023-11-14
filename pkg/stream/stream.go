1ab9c6c22c65ada5d7980bf1f843a87e4b75c1d2
Mikita Iwanowski <info@slnt-opp.xyz>
2023-11-14 15:41:39 +0100

    add debug log on "is bot"

--- pkg/stream/stream.go
+++ pkg/stream/stream.go
@@ -73,6 +73,7 @@ func (s *StreamServer) Stream(ctx context.Context, req *connect.Request[cc.Strea
 start_stream:
 
 	if streamUser.GetCcIsBot() {
+		log.Debug("Stream requestor is a bot", zap.String("username", streamUser.GetCcUsername()), zap.Any("commands", req.Msg.GetCommands()))
 		err := s.userCtrl.UpdateCommands(ctx, streamUser.GetUuid(), req.Msg.GetCommands())
 		if err != nil {
 			log.Error("Failed to update commands in bot", zap.Error(err))

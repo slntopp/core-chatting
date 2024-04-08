685a69e80d56ca792e083ddf31a328dffe04d4af
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2024-04-08 21:51:29 +0300

    Create atachments controller

---
+++ pkg/graph/attachments.go
@@ -0,0 +1,77 @@
+package graph
+
+import (
+	"context"
+
+	"github.com/arangodb/go-driver"
+	"go.uber.org/zap"
+)
+
+type Attachment struct {
+	Uuid    string `json:"uuid"`
+	Title   string `json:"title"`
+	Message string `json:"message"`
+}
+
+type AttachmentsController struct {
+	log *zap.Logger
+
+	db  driver.Database
+	col driver.Collection
+}
+
+func NewAttachmentsController(logger *zap.Logger, db driver.Database) *AttachmentsController {
+	log := logger.Named("Attachments")
+	log.Debug("Creating attachments controller")
+
+	ctx := context.TODO()
+	col := GetEnsureCollection(log, ctx, db, ATTACHMENTS_COLLECTION)
+
+	return &AttachmentsController{
+		log: log,
+		db:  db,
+		col: col,
+	}
+}
+
+func (c *AttachmentsController) Upload(ctx context.Context, a *Attachment) (*Attachment, error) {
+	log := c.log.Named("Upload")
+	document, err := c.col.CreateDocument(ctx, a)
+	if err != nil {
+		log.Error("Failed to create document", zap.Error(err))
+		return nil, err
+	}
+
+	a.Uuid = document.Key
+
+	_, err = c.col.UpdateDocument(ctx, a.Uuid, a)
+	if err != nil {
+		c.col.RemoveDocument(ctx, a.Uuid)
+		log.Error("Failed to update document", zap.Error(err))
+		return nil, err
+	}
+	return a, nil
+}
+
+func (c *AttachmentsController) Get(ctx context.Context, uuid string) (*Attachment, error) {
+	log := c.log.Named("Get")
+
+	var attachment Attachment
+	_, err := c.col.ReadDocument(ctx, uuid, &attachment)
+	if err != nil {
+		log.Error("Failed to read document", zap.Error(err))
+		return nil, err
+	}
+	return &attachment, nil
+}
+
+func (c *AttachmentsController) Delete(ctx context.Context, uuid string) error {
+	log := c.log.Named("Get")
+
+	_, err := c.col.RemoveDocument(ctx, uuid)
+	if err != nil {
+		log.Error("Failed to delete document", zap.Error(err))
+		return err
+	}
+	return nil
+}
--- pkg/graph/schema.go
+++ pkg/graph/schema.go
@@ -1,6 +1,7 @@
 package graph
 
 const (
-	CHATS_COLLECTION    = "CC_Chats"
-	MESSAGES_COLLECTION = "CC_Messages"
+	CHATS_COLLECTION       = "CC_Chats"
+	MESSAGES_COLLECTION    = "CC_Messages"
+	ATTACHMENTS_COLLECTION = "CC_Attachments"
 )

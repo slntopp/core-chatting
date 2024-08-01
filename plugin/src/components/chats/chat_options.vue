3984a3d5e9c75151949290412a9ae11ecb68bccb
SazukinPavel <sazukinpavelal@gmail.com>
2024-08-01 14:02:52 +0400

    add department in chat options

--- plugin/src/components/chats/chat_options.vue
+++ plugin/src/components/chats/chat_options.vue
@@ -3,36 +3,64 @@
     <n-space v-if="!isEdit" justify="start" align="center">
       <n-button round quaternary @click="cancel">
         <template #icon>
-          <n-icon :component="CloseSharp"/>
+          <n-icon :component="CloseSharp" />
         </template>
       </n-button>
 
-      <n-text>
-        Start a new Chat
-      </n-text>
+      <n-text> Start a new Chat </n-text>
     </n-space>
 
-    <n-space vertical justify="start" style="max-width: 800px; margin: auto; width: 100%">
+    <n-space
+      vertical
+      justify="start"
+      style="max-width: 800px; margin: auto; width: 100%"
+    >
       <n-form :model="chat" ref="form" :rules="rules" label-placement="left">
-        <n-form-item label="Topic" label-align="left" label-width="75">
-          <n-input v-model:value="chat.topic" clearable placeholder="What are we chatting about?"/>
+        <n-form-item label="Topic" label-align="left" label-width="100">
+          <n-input
+            v-model:value="chat.topic"
+            clearable
+            placeholder="What are we chatting about?"
+          />
         </n-form-item>
 
-        <n-form-item label="Members" label-align="left" label-width="75">
-          <member-select v-model:value="chat.users" :options="membersWithoutDuplicates"/>
+        <n-form-item label="Members" label-align="left" label-width="100">
+          <member-select
+            v-model:value="chat.users"
+            :options="membersWithoutDuplicates"
+          />
         </n-form-item>
 
-        <n-form-item label="Admins" label-align="left" label-width="75">
-          <n-select v-model:value="chat.admins" multiple :options="adminsWithoutDuplicates" filterable/>
+        <n-form-item label="Departament" label-align="left" label-width="100">
+          <n-select
+            v-model:value="chat.department"
+            :options="departamentsOptions"
+            filterable
+            clearable
+          />
         </n-form-item>
 
-        <n-form-item label="Gateways" label-align="left" label-width="75">
-          <n-select v-model:value="chat.gateways" multiple :options="gateways_options" filterable/>
+        <n-form-item label="Admins" label-align="left" label-width="100">
+          <n-select
+            v-model:value="chat.admins"
+            multiple
+            :options="adminsWithoutDuplicates"
+            filterable
+          />
+        </n-form-item>
+
+        <n-form-item label="Gateways" label-align="left" label-width="100">
+          <n-select
+            v-model:value="chat.gateways"
+            multiple
+            :options="gateways_options"
+            filterable
+          />
         </n-form-item>
 
         <n-form-item
           label-align="left"
-          label-width="75"
+          label-width="100"
           v-for="metric of metrics"
           :label="metric.title"
           :key="metric.key"
@@ -47,18 +75,34 @@
         </n-form-item>
 
         <n-space justify="end">
-          <n-button :loading="isEditLoading" ghost type="success" @click="submit">
-            {{ isEdit ? 'Save' : 'Start chat' }}
+          <n-button
+            :loading="isEditLoading"
+            ghost
+            type="success"
+            @click="submit"
+          >
+            {{ isEdit ? "Save" : "Start chat" }}
           </n-button>
         </n-space>
       </n-form>
     </n-space>
   </div>
-  <n-spin style="width: 100%;height: 100%; margin: auto" size="large" v-else></n-spin>
+  <n-spin
+    style="width: 100%; height: 100%; margin: auto"
+    size="large"
+    v-else
+  ></n-spin>
 </template>
 
 <script setup lang="ts">
-import {computed, defineAsyncComponent, onMounted, ref, toRefs} from 'vue';
+import {
+  computed,
+  defineAsyncComponent,
+  onMounted,
+  ref,
+  toRefs,
+  watch,
+} from "vue";
 import {
   FormInst,
   NButton,
@@ -71,153 +115,197 @@ import {
   NSpin,
   NText,
   SelectOption,
-} from 'naive-ui';
+} from "naive-ui";
 
-import {useRouter} from 'vue-router';
-import {useCcStore} from "../../store/chatting.ts";
-import {useAppStore} from '../../store/app.ts';
-import {Chat, ChatMeta, Role} from "../../connect/cc/cc_pb";
+import { useRouter } from "vue-router";
+import { useCcStore } from "../../store/chatting.ts";
+import { useAppStore } from "../../store/app.ts";
+import { Chat, ChatMeta, Role } from "../../connect/cc/cc_pb";
 import useDefaults from "../../hooks/useDefaults.ts";
 import MemberSelect from "../users/member_select.vue";
-import { ValueAtom } from 'naive-ui/es/select/src/interface';
-import { Value } from '@bufbuild/protobuf';
+import { ValueAtom } from "naive-ui/es/select/src/interface";
+import { Value } from "@bufbuild/protobuf";
 
 interface ChatOptionsProps {
-  minHeight?: string
-  isEdit?: boolean
-  chat?: Chat
+  minHeight?: string;
+  isEdit?: boolean;
+  chat?: Chat;
 }
 
-const CloseSharp = defineAsyncComponent(() => import('@vicons/ionicons5/CloseSharp'));
+const CloseSharp = defineAsyncComponent(
+  () => import("@vicons/ionicons5/CloseSharp")
+);
 
-const props = defineProps<ChatOptionsProps>()
-const {isEdit, chat: oldChat} = toRefs(props)
+const props = defineProps<ChatOptionsProps>();
+const { isEdit, chat: oldChat } = toRefs(props);
 
-const emit = defineEmits(['close'])
+const emit = defineEmits(["close"]);
 
-const router = useRouter()
-const store = useCcStore()
-const appStore = useAppStore()
-const {admins, fetch_defaults, isDefaultLoading, users, gateways, metrics} = useDefaults()
+const router = useRouter();
+const store = useCcStore();
+const appStore = useAppStore();
+const {
+  admins,
+  fetch_defaults,
+  isDefaultLoading,
+  users,
+  gateways,
+  metrics,
+  departments,
+} = useDefaults();
 
-const form = ref<FormInst>()
+const form = ref<FormInst>();
 const rules = {
   members: {
-    required: true
-  }
-}
+    required: true,
+  },
+};
 
-const isEditLoading = ref(false)
+const isEditLoading = ref(false);
 
-const chat = ref<Chat>(new Chat({
-  uuid: '', topic: '', users: [store.me.uuid],
-  admins: [], gateways: [],
-  role: Role.OWNER,
-}))
+const chat = ref<Chat>(
+  new Chat({
+    uuid: "",
+    topic: "",
+    users: [store.me.uuid],
+    admins: [],
+    gateways: [],
+    role: Role.OWNER,
+  })
+);
 
-window.addEventListener('message', ({ data, origin }) => {
-  if (origin.includes('localhost')) return
-  if (data.type !== 'user-uuid') return
-  chat.value.users.push(data.value)
-})
+window.addEventListener("message", ({ data, origin }) => {
+  if (origin.includes("localhost")) return;
+  if (data.type !== "user-uuid") return;
+  chat.value.users.push(data.value);
+});
 
-const admins_options = ref<SelectOption[]>([])
-const gateways_options = ref<SelectOption[]>([])
-const members_options = ref<SelectOption[]>([])
+const admins_options = ref<SelectOption[]>([]);
+const gateways_options = ref<SelectOption[]>([]);
+const members_options = ref<SelectOption[]>([]);
 
-const adminsWithoutDuplicates = computed(() => admins_options.value.map(op => {
-  const disabled = !!chat.value.users.find(m => m === op.value)
-  return {...op, disabled}
-}))
+const adminsWithoutDuplicates = computed(() =>
+  admins_options.value.map((op) => {
+    const disabled = !!chat.value.users.find((m) => m === op.value);
+    return { ...op, disabled };
+  })
+);
 
-const membersWithoutDuplicates = computed(() => members_options.value.map(op => {
-  const disabled = !!chat.value.admins.find(m => m === op.value)
-  return {...op, disabled}
-}))
+const membersWithoutDuplicates = computed(() =>
+  members_options.value.map((op) => {
+    const disabled = !!chat.value.admins.find((m) => m === op.value);
+    return { ...op, disabled };
+  })
+);
 
-const me = computed(() => store.me)
+const departamentsOptions = computed(() =>
+  departments.value.map((departament) => ({
+    label: departament.title,
+    value: departament.key,
+  }))
+);
+
+const me = computed(() => store.me);
 
 onMounted(() => {
   if (isEdit?.value && oldChat?.value) {
-    chat.value = {...oldChat.value} as Chat
+    chat.value = { ...oldChat.value } as Chat;
   }
-  window.top?.postMessage({ type: 'get-user' }, '*')
-})
+  window.top?.postMessage({ type: "get-user" }, "*");
+});
 
 onMounted(async () => {
-  await fetch_defaults()
+  await fetch_defaults();
 
-  admins_options.value = admins.value.map(admin => {
+  admins_options.value = admins.value.map((admin) => {
     return {
-      label: store.users.get(admin)?.title ?? 'Unknown',
+      label: store.users.get(admin)?.title ?? "Unknown",
       value: admin,
-    }
-  })
+    };
+  });
 
-  gateways_options.value = gateways.value.map(gateway => {
+  gateways_options.value = gateways.value.map((gateway) => {
     return {
       label: gateway,
       value: gateway,
-    }
-  })
-  chat.value.gateways = gateways.value
+    };
+  });
+  chat.value.gateways = gateways.value;
 
-  members_options.value = users.value.map(user => {
+  members_options.value = users.value.map((user) => {
     return {
       label: user.title,
       value: user.uuid,
       disabled: me.value.uuid !== user.uuid && admins.value.includes(user.uuid),
-    }
-  })
-})
+    };
+  });
+});
 
 function submit() {
   form.value!.validate(async (errs) => {
     if (errs) {
-      console.error("Errors", errs)
-      return
+      console.error("Errors", errs);
+      return;
     }
 
     try {
-      isEditLoading.value = true
+      isEditLoading.value = true;
 
       if (isEdit?.value) {
-        await store.update_chat(chat.value as Chat)
+        await store.update_chat(chat.value as Chat);
 
-        emit('close')
+        emit("close");
       } else {
-        let { uuid } = await store.create_chat(chat.value as Chat)
+        let { uuid } = await store.create_chat(chat.value as Chat);
 
-        router.push({name: 'Chat', params: {uuid}})
+        router.push({ name: "Chat", params: { uuid } });
       }
     } finally {
-      isEditLoading.value = false
+      isEditLoading.value = false;
     }
-  })
+  });
 }
 
 function getMetric(key: string) {
-  return chat.value.meta?.data[key]?.kind.value as ValueAtom
+  return chat.value.meta?.data[key]?.kind.value as ValueAtom;
 }
 
 function setMetric(value: number, key: string) {
-  if (!chat.value.meta) chat.value.meta = new ChatMeta({})
+  if (!chat.value.meta) chat.value.meta = new ChatMeta({});
 
   chat.value.meta.data[key] = new Value({
-    kind: { case: 'numberValue', value }
-  })
+    kind: { case: "numberValue", value },
+  });
 }
 
 function cancel() {
-  router.push({ name: 'Empty Chat' })
-  appStore.displayMode = 'half'
+  router.push({ name: "Empty Chat" });
+  appStore.displayMode = "half";
 }
+
+watch(
+  () => chat.value.department,
+  () => {
+    if (isEdit.value) {
+      return;
+    }
+
+    if (chat.value.department) {
+      const departament = departments.value.find(
+        (departament) => departament.key === chat.value.department
+      );
+      chat.value.admins = departament?.admins || [];
+    } else {
+      chat.value.admins = [];
+    }
+  }
+);
 </script>
 
 <style scoped>
 .chat-options {
   display: flex;
   flex-direction: column;
-  min-height: v-bind('minHeight');
+  min-height: v-bind("minHeight");
 }
 </style>
